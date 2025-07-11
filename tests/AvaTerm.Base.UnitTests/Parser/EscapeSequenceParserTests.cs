128ce2b559119e2b61a6ef6a80ae618906f50dba
LeLeMoe <lele@lele.moe>
2025-07-12 00:44:05 +0800

    Update test data for EscapeSequenceParser tests
    Updated test data for EscapeSequenceParser tests.
    Performed EscapeSequenceParser tests in legacy mode and Unicode mode respectively.

--- tests/AvaTerm.Base.UnitTests/Parser/EscapeSequenceParserTests.cs
+++ tests/AvaTerm.Base.UnitTests/Parser/EscapeSequenceParserTests.cs
@@ -318,34 +318,26 @@ public class EscapeSequenceParserTests
 
     public static TheoryData<char, byte> GetMapCodeTestDataForLegacyMode()
     {
-        return new TheoryData<char, byte>
+        var data = new TheoryData<char, byte>();
+
+        // Invalid Unicode characters
+        data.Add('\u0100', 0x00);
+        data.Add('\uFFFF', 0x00);
+        data.Add('\u5948', 0x00);
+
+        // All characters in GR area
+        for (var code = '\u00A0'; code <= '\u00FF'; ++code)
         {
-            // Invalid Unicode characters
-            { '\u0100', 0x00 },
-            { '\uFFFF', 0x00 },
-            { '\uF90C', 0x00 },
-            { '\u306A', 0x00 },
+            data.Add(code, (byte)(code - 0x0080));
+        }
 
-            // Characters in GR area
-            { '\u00A0', 0x20 },
-            { '\u00FF', 0x7F },
-            { '\u00C1', 0x41 },
-            { '\u00FA', 0x7A },
+        // All characters in C0, GL and C1 areas
+        for (var code = '\u0000'; code <= '\u009F'; ++code)
+        {
+            data.Add(code, (byte)code);
+        }
 
-            // Characters in C0, C1 and GL areas
-            { '\u0000', 0x00 },
-            { '\u001F', 0x1F },
-            { '\u0009', 0x09 },
-            { '\u001B', 0x1B },
-            { '\u0080', 0x80 },
-            { '\u009F', 0x9F },
-            { '\u0084', 0x84 },
-            { '\u009B', 0x9B },
-            { '\u0020', 0x20 },
-            { '\u007F', 0x7F },
-            { '\u0041', 0x41 },
-            { '\u007A', 0x7A },
-        };
+        return data;
     }
 
     [Theory]
@@ -371,28 +363,20 @@ public class EscapeSequenceParserTests
 
     public static TheoryData<char, byte> GetMapCodeTestDataForUnicodeMode()
     {
-        return new TheoryData<char, byte>
-        {
-            // Unicode characters
-            { '\u00A0', 0xA0 },
-            { '\uFFFF', 0xA0 },
-            { '\u00BC', 0xA0 },
-            { '\u306A', 0xA0 },
+        var data = new TheoryData<char, byte>();
 
-            // Characters in C0, C1 and GL areas
-            { '\u0000', 0x00 },
-            { '\u001F', 0x1F },
-            { '\u0009', 0x09 },
-            { '\u001B', 0x1B },
-            { '\u0080', 0x80 },
-            { '\u009F', 0x9F },
-            { '\u0084', 0x84 },
-            { '\u009B', 0x9B },
-            { '\u0020', 0x20 },
-            { '\u007F', 0x7F },
-            { '\u0041', 0x41 },
-            { '\u007A', 0x7A },
-        };
+        // Unicode characters
+        data.Add('\u00A0', 0xA0);
+        data.Add('\uFFFF', 0xA0);
+        data.Add('\u5948', 0xA0);
+
+        // All characters in C0, GL and C1 areas
+        for (var code = '\u0000'; code <= '\u009F'; ++code)
+        {
+            data.Add(code, (byte)code);
+        }
+
+        return data;
     }
 
     [Theory]
@@ -416,13 +400,81 @@ public class EscapeSequenceParserTests
         Assert.Equal(mappedCode, resultValue);
     }
 
-    [Theory]
-    [InlineData("\u0020")]
-    [InlineData("\u007F")]
-    [InlineData("Hello, World!")]
-    public void Parse_PrintableInput_CallPrintAction(string text)
+    public static TheoryData<string> GetPrintableInputTestDataForLegacyMode()
     {
-        var parser = new EscapeSequenceParser();
+        var data = new TheoryData<string>();
+
+        // Characters in GL area
+        for (var code = '\u0020'; code <= '\u007F'; ++code)
+        {
+            data.Add(code.ToString());
+        }
+
+        // Characters in GR area
+        for (var code = '\u00A0'; code <= '\u00FF'; ++code)
+        {
+            data.Add(code.ToString());
+        }
+
+        // Consecutive valid characters
+        data.Add("The quick brown fox jumps over the lazy dog.");
+        data.Add("\u00AE\u00F9\u00E4\u00E0\u00E8\u00A9\u00BB\u00F5\u00AE\u00DC\u00C8");
+        data.Add("\u00DB\u007A\u00CB\u00F0\u0068\u0061\u006A\u00E6\u0040\u0031");
+        data.Add("\u007F\u007F\u00FF\u00A0\u00FF\u0020\u00FF\u007F");
+
+        return data;
+    }
+
+    [Theory]
+    [MemberData(nameof(GetPrintableInputTestDataForLegacyMode))]
+    public void Parse_PrintableInputInLegacyMode_CallPrintAction(string text)
+    {
+        var parser = new EscapeSequenceParser
+        {
+            LegacyMode = true
+        };
+        var handler = new MockHandler(parser);
+
+        parser.Parse(text);
+        var result = handler.Actions;
+
+        var actual = new List<ActionRecord> { new ActionRecord("print", text) };
+        Assert.True(result.SequenceEqual(actual));
+    }
+
+    public static TheoryData<string> GetPrintableInputTestDataFoUnicodeMode()
+    {
+        var data = new TheoryData<string>();
+
+        // Characters in GL area
+        for (var code = '\u0020'; code <= '\u007F'; ++code)
+        {
+            data.Add(code.ToString());
+        }
+
+        // Single Unicode character
+        data.Add("\u00A0");
+        data.Add("\uFFFF");
+        data.Add("\u5948");
+        data.Add("\uD616");
+
+        // Consecutive valid characters
+        data.Add("The quick brown fox jumps over the lazy dog.");
+        data.Add("\u46CD\u3635\uF068\uB935\u0801\u2F68\uF30C\uA176");
+        data.Add("\uC65B\u0023\u0FB7\u04B5\uE5D1\uBBFB\u007C\u8549\u6F38\u1246\u006E");
+        data.Add("\uFFFF\u0020\u0020\uFFFF\uFFFF\u007F\u00A0\u007F");
+
+        return data;
+    }
+
+    [Theory]
+    [MemberData(nameof(GetPrintableInputTestDataFoUnicodeMode))]
+    public void Parse_PrintableInputInUnicodeMode_CallPrintAction(string text)
+    {
+        var parser = new EscapeSequenceParser
+        {
+            LegacyMode = false
+        };
         var handler = new MockHandler(parser);
 
         parser.Parse(text);
